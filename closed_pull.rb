7070cb17f2f0d1e58bd9e46a72cbfb2b932e661b
judy johnson <judy@localhost.localdomain>
2018-09-08 18:18:11 -0400

    allowing over 50 at once, cleaned up

--- closed_pull.rb
+++ closed_pull.rb
@@ -1,100 +1,84 @@
-# this does a pull of the closed tickets 
-# one with parent/ one without
-# the query is closed <= 16d
-# (you may need to change it based on your sprint length - or just edit out what you don't need)
-# you generally have to de-dupe it with what's already in there anyhow
-#
 require 'rest-client'
 require 'json'
 
 # first create two .csv files - one with the parent appended, the other without - ensure the field names are OK
-pullfile = File.open("closedpull.csv", "w")
-parentpullfile = File.open("closedpull_wparent.csv", "w")
-pullfile.puts( "Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee")
-parentpullfile.puts("Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee")
+parentpullfile = File.open('closed_pull.csv', 'w')
+parentpullfile.puts('Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee')
 
 # here is our jira instance
-project_key = "ABC"
-jira_url = "https://simp-project.atlassian.net/rest/api/2/search?"
+project_key = 'ABC'
+jira_url = 'https://simp-project.atlassian.net/rest/api/2/search?'
 
-#find current sprint
-filter = "jql=status%3dClosed%20and%20resolved%3e%2d16d"
+# find current sprint
+filter = "jql=status%3dClosed%20and%20resolved%3e%2d14d"
 
 # set a max # results - defaults to 50 (we can switch this to a loop later)
-maxresults = 250
-filter = "#{filter}&maxResults=#{maxresults}"
+total_issues = 1
+ticket_count = 0
+maxresults = 50
+newfilter = "#{filter}&maxResults=#{maxresults}&startAt=#{total_issues}"
 
-# call the code
-response = RestClient.get(jira_url+filter)
-if(response.code != 200)
-  raise "Error with the http request!"
+# while we have tickets still
+while ticket_count < total_issues
+
+  # call the code
+  newfilter = "#{filter}&maxResults=#{maxresults}&startAt=#{ticket_count}"
+  response = RestClient.get(jira_url + newfilter)
+  raise 'Error with the http request!' if response.code != 200
+
+  data = JSON.parse(response.body)
+  # puts "current data is #{data}"
+  # find the number of tickets returned
+  total_issues = data['total']
+
+  data['issues'].each do |issue|
+    points = issue['fields']['customfield_10005']
+    points = points.to_i
+    issuekey = issue['key']
+    summary = issue['fields']['summary']
+    desc = issue['fields']['description']
+    # substitute apostrophe for quote
+    unless desc.nil?
+      temp = desc.tr('"', "\'")
+      desc = temp
+    end
+
+    # see if it has a parent, and if so, display it"
+    parent = if !issue['fields']['parent'].nil?
+               issue['fields']['parent']['key']
+             else
+               "#{issuekey}."
+             end
+
+    # calculate the sprint by breaking the "sprint=" out of the sprint attributes string
+    sprintdata = issue['fields']['customfield_10007']
+    if sprintdata.size > 0
+      idstring = sprintdata[0]
+      idstringname = idstring.slice(idstring.index('name='), idstring.size)
+      comma = idstringname.index(',') - 1
+      sprintid = idstringname[5..comma]
+    else
+      sprintid = ''
+    end
+
+    # get type
+    issuetype = if !issue['fields']['issuetype'].nil?
+                  issue['fields']['issuetype']['name']
+                else
+                  ''
+                end
+
+    # get assignee
+    assignee = if !issue['fields']['assignee'].nil?
+                 issue['fields']['assignee']['name']
+               else
+                 ''
+               end
+
+    # write to files
+    parentpullfile.puts("#{issuekey},#{parent},\"#{parent}/#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}")
+    ticket_count = ticket_count + 1
+
+  end # while there are still tickets
+  puts "ticket count is #{ticket_count}"
 end
-
-data = JSON.parse(response.body)
-#puts "current data is #{data}"
-
-data['issues'].each do |issue|
-  points = issue['fields']['customfield_10005']
-  points = points.to_i
-  issuekey = issue['key']
-  summary = issue['fields']['summary']
-  desc = issue['fields']['description']
-  if (desc != nil)
-    temp = desc.gsub("\"","\'")
-    desc = temp
-  end
-
-  # see if it has a parent, and if so, display it"
-  if issue['fields']['parent'] != nil
-    parent = issue['fields']['parent']['key']
-  else
-    parent = "#{issuekey}."
-  end
-
-  # calculate the sprint by breaking the "sprint=" out of the sprint attributes string 
-  sprintdata = issue['fields']['customfield_10007']
-  if sprintdata.size > 0
-    idstring = sprintdata[0]
-    idstringname = idstring.slice(idstring.index('name='),idstring.size)
-    comma=idstringname.index(',')-1
-    sprintid=idstringname[5..comma]
-  else
-    sprintid = ""
-  end
-
-  # get type
-  if issue['fields']['issuetype'] != nil
-    issuetype = issue['fields']['issuetype']['name']
-  else
-    issuetype = ""
-  end
-
-  # get assignee
-  if issue['fields']['assignee'] != nil
-    assignee = issue['fields']['assignee']['name']
-  else
-    assignee = ""
-  end
-
-  # get status
-  if issue['fields']['status'] != nil
-    status = issue['fields']['status']['name']
-  else
-    status = ""
-  end
- 
-  # write to files 
-  pullfile.puts( "#{issuekey},#{parent},\"#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}" )
-  parentpullfile.puts( "#{issuekey},#{parent},\"#{parent}/#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}" )
-
-
-  # here for later -- if we get stuck and need to find another issue attribute, use this as a starting point 
-  issue['fields'].each do |ifield|
-#    puts "field: #{ifield}"
-#    puts "value: #issue['fields'][ifield]"
-  end
-end
-
-
-
-
--- current_pull.rb
+++ current_pull.rb
@@ -1,95 +1,84 @@
-# jira_pull.rb 
-# this file pulls the jira tickets for the CURRENT sprint 
-# and writes the data to a comma-separated file
-# note that we get 2 files - one just has the tasks, 
-# the second _wparent appends the parent with a slash
-# so description on jirapull.csv is "task 1"
-# and on jirapull_wparent.csv is "parent/task 1"
 require 'rest-client'
 require 'json'
 
 # first create two .csv files - one with the parent appended, the other without - ensure the field names are OK
-pullfile = File.open("jirapull.csv", "w")
-parentpullfile = File.open("jirapull_wparent.csv", "w")
-pullfile.puts( "Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee")
-parentpullfile.puts("Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee")
+parentpullfile = File.open('jirapull.csv', 'w')
+parentpullfile.puts('Issue id,Parent id,Summary,Issue Type,Story Points,Sprint,Description,Assignee')
 
 # here is our jira instance
-project_key = "ABC"
-jira_url = "https://simp-project.atlassian.net/rest/api/2/search?"
+project_key = 'ABC'
+jira_url = 'https://simp-project.atlassian.net/rest/api/2/search?'
 
-#find current sprint
-filter = "jql=sprint%20in%20openSprints()"
+# find current sprint
+filter = 'jql=sprint%20in%20openSprints()'
 
 # set a max # results - defaults to 50 (we can switch this to a loop later)
-maxresults = 250
-filter = "#{filter}&maxResults=#{maxresults}"
+total_issues = 1
+ticket_count = 0
+maxresults = 50
+newfilter = "#{filter}&maxResults=#{maxresults}&startAt=#{total_issues}"
 
-# call the code
-response = RestClient.get(jira_url+filter)
-if(response.code != 200)
-  raise "Error with the http request!"
+# while we have tickets still
+while ticket_count < total_issues
+
+  # call the code
+  newfilter = "#{filter}&maxResults=#{maxresults}&startAt=#{ticket_count}"
+  response = RestClient.get(jira_url + newfilter)
+  raise 'Error with the http request!' if response.code != 200
+
+  data = JSON.parse(response.body)
+  # puts "current data is #{data}"
+  # find the number of tickets returned
+  total_issues = data['total']
+
+  data['issues'].each do |issue|
+    points = issue['fields']['customfield_10005']
+    points = points.to_i
+    issuekey = issue['key']
+    summary = issue['fields']['summary']
+    desc = issue['fields']['description']
+    # substitute apostrophe for quote
+    unless desc.nil?
+      temp = desc.tr('"', "\'")
+      desc = temp
+    end
+
+    # see if it has a parent, and if so, display it"
+    parent = if !issue['fields']['parent'].nil?
+               issue['fields']['parent']['key']
+             else
+               "#{issuekey}."
+             end
+
+    # calculate the sprint by breaking the "sprint=" out of the sprint attributes string
+    sprintdata = issue['fields']['customfield_10007']
+    if sprintdata.size > 0
+      idstring = sprintdata[0]
+      idstringname = idstring.slice(idstring.index('name='), idstring.size)
+      comma = idstringname.index(',') - 1
+      sprintid = idstringname[5..comma]
+    else
+      sprintid = ''
+    end
+
+    # get type
+    issuetype = if !issue['fields']['issuetype'].nil?
+                  issue['fields']['issuetype']['name']
+                else
+                  ''
+                end
+
+    # get assignee
+    assignee = if !issue['fields']['assignee'].nil?
+                 issue['fields']['assignee']['name']
+               else
+                 ''
+               end
+
+    # write to files
+    parentpullfile.puts("#{issuekey},#{parent},\"#{parent}/#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}")
+    ticket_count = ticket_count + 1
+
+  end # while there are still tickets
+  puts "ticket count is #{ticket_count}"
 end
-
-data = JSON.parse(response.body)
-#puts "current data is #{data}"
-
-data['issues'].each do |issue|
-  points = issue['fields']['customfield_10005']
-  points = points.to_i
-  issuekey = issue['key']
-  summary = issue['fields']['summary']
-  desc = issue['fields']['description']
-  # substitute apostrophe for quote
-  if (desc != nil)
-    temp = desc.gsub("\"","\'")
-    desc = temp
-  end
-
-  # see if it has a parent, and if so, display it"
-  if issue['fields']['parent'] != nil
-    parent = issue['fields']['parent']['key']
-  else
-    parent = "#{issuekey}."
-  end
-
-  # calculate the sprint by breaking the "sprint=" out of the sprint attributes string 
-  sprintdata = issue['fields']['customfield_10007']
-  if sprintdata != nil
-    idstring = sprintdata[0]
-    idstringname = idstring.slice(idstring.index('name='),idstring.size)
-    comma=idstringname.index(',')-1
-    sprintid=idstringname[5..comma]
-  else
-    sprintid = ""
-  end
-
-  # get type
-  if issue['fields']['issuetype'] != nil
-    issuetype = issue['fields']['issuetype']['name']
-  else
-    issuetype = ""
-  end
-
-  # get assignee
-  if issue['fields']['assignee'] != nil
-    assignee = issue['fields']['assignee']['name']
-  else
-    assignee = ""
-  end
-
-  # write to files 
-  pullfile.puts( "#{issuekey},#{parent},\"#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}" )
-  parentpullfile.puts( "#{issuekey},#{parent},\"#{parent}/#{summary} (#{issuekey})\",#{issuetype},#{points},#{sprintid},\"#{desc}\",#{assignee}" )
-
-
-  # here for later -- if we get stuck and need to find another issue attribute, use this as a starting point 
-  issue['fields'].each do |ifield|
-#    puts "field: #{ifield}"
-#    puts "value: #issue['fields'][ifield]"
-  end
-end
-
-
-
-
