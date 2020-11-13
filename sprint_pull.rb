230beb57597e49510d23aba6fdb6519a3fc1f64d
Judy Johnson <judyfinkjohnson@gmail.com>
2020-11-13 19:53:03 +0000

    fixed json bug

--- sprint_pull.rb
+++ sprint_pull.rb
@@ -67,9 +67,7 @@ while ticket_count < total_issues
     sprintdata = issue['fields']['customfield_10007']
     if sprintdata != nil
       idstring = sprintdata[0]
-      idstringname = idstring.slice(idstring.index('name='), idstring.size)
-      comma = idstringname.index(',') - 1
-      sprintid = idstringname[5..comma]
+      sprintid = idstring['name']
     else
       sprintid = ''
     end
@@ -81,7 +79,7 @@ while ticket_count < total_issues
     else
       component = ''
     end
-    puts "component is #{component}"
+    # puts "component is #{component}"
 
     # get type
     issuetype = if !issue['fields']['issuetype'].nil?
